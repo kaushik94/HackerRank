5928c4b124d12a3f5262602bccde5e0e0471a6fa
kaushik <kaushik.varanasi1@gmail.com>
2014-04-14 00:42:45 +0530

    implemented all algorithms

--- pacman.py
+++ pacman.py
@@ -12,7 +12,8 @@ Route = []
 
 def printer():
 
-    print len(printList)
+    if(printList):
+    	print len(printList)
     while(printList):
         p = printList.pop(0)
         print p[0],
@@ -24,12 +25,70 @@ def printer():
         print p[0],
         print p[1]
 
+def ManhattanHeuristic(A, B):
+
+	X1, Y1 = A
+	X2, Y2 = B
+
+	return abs(X1-X2) + abs(Y1-Y2)
 
 def nextMove(r, c, pacman_r, pacman_c, food_r, food_c, grid):
 
-    BFS(r, c, pacman_r, pacman_c, food_r, food_c, grid)
-    #DFS(r, c, pacman_r, pacman_c, food_r, food_c, grid)
+    # BFS(r, c, pacman_r, pacman_c, food_r, food_c, grid)
+    # DFS(r, c, pacman_r, pacman_c, food_r, food_c, grid)
+    # UCS(r, c, pacman_r, pacman_c, food_r, food_c, grid)
+    AStar(r, c, pacman_r, pacman_c, food_r, food_c, grid)
 
+
+def UCS(r, c, pacman_r, pacman_c, food_r, food_c, grid):
+
+    ToVisit = PriorityQueue()
+    Food.append(tuple((food_r, food_c)))
+    cost = 0
+    ToVisit.put(tuple((tuple((pacman_r, pacman_c)), cost)), cost)
+    ToVisit_.append(tuple((tuple((pacman_r, pacman_c)), cost)))
+
+    while(ToVisit):
+        visit = ToVisit.get()
+	visit_cost = visit[1]
+        found = 0
+
+	if visit[0] in Food:
+		food_r, food_c = Food.pop()
+		Food.append((tuple((food_r, food_c)) , visit_cost))
+		break
+
+	for i in Visited:
+		if(i[0] == visit[0]):
+			found = 1
+			if(i[1] > visit_cost):
+				Visited.remove(i)
+				children = getNeighbours(visit[0], r, c)
+				for child in children:
+					child = tuple((child, visit_cost + 1))
+					X[child] = visit
+					ToVisit.put(child, visit_cost+1)
+				Visited.append(visit)
+
+			else:
+				continue
+
+	if(found == 0):
+		children = getNeighbours(visit[0], r, c)
+		for child in children:
+			child = tuple((child, visit_cost + 1))
+			X[child] = visit
+			ToVisit.put(child, visit_cost+1)
+		Visited.append(visit)
+
+    node  = Food.pop()
+    start = tuple((tuple((pacman_r, pacman_c)), 0))
+    while(node[0] != start[0]):
+	Route.append(node[0])
+	node = X[node]
+
+    Route.append(start[0])
+    printer()
 
 def BFS(r, c, pacman_r, pacman_c, food_r, food_c, grid):
 
@@ -66,76 +125,55 @@ def BFS(r, c, pacman_r, pacman_c, food_r, food_c, grid):
     Route.append(start)
     printer()
 
-
-def dfs(r, c, pacman_r, pacman_c, food_r, food_c, grid):
-
-    ToVisit = []
-    Food.append(tuple((food_r, food_c)))
-    ToVisit.append(tuple((pacman_r, pacman_c)))
-    ToVisit_.append(tuple((pacman_r, pacman_c)))
-
-    while(ToVisit):
-        visit = ToVisit.pop()
-        ToVisit_.pop()
-        printList.append(visit)
-        if visit in Food:
-            # print "found"
-            break
-
-        if visit not in Visited:
-            Visited.append(visit)
-            children = getNeighbours(visit, r, c)
-            # print children
-            while (children):
-                child = children.pop(0)
-                if child not in Visited:
-                    if child not in ToVisit_:
-                        ToVisit.append(child)
-                        ToVisit_.append(child)
-                        X[child] = visit
-
-    node = Food.pop()
-    start = tuple((pacman_r, pacman_c))
-    while(node != start):
-        Route.append(node)
-        node = X[node]
-    Route.append(start)
-    printer()
-
-
 def AStar(r, c, pacman_r, pacman_c, food_r, food_c, grid):
 
     ToVisit = PriorityQueue()
-    Food.append(tuple((food_r, food_c)))
-    ToVisit.put(tuple((pacman_r, pacman_c)))
-    ToVisit_.append(tuple((pacman_r, pacman_c)))
+    goal = tuple((food_r, food_c))
+    Food.append(goal)
+    cost = 0
+    ToVisit.put(tuple((tuple((pacman_r, pacman_c)), cost)), cost)
+    ToVisit_.append(tuple((tuple((pacman_r, pacman_c)), cost)))
 
     while(ToVisit):
         visit = ToVisit.get()
-        ToVisit_.pop(0)
-        printList.append(visit)
-        if visit in Food:
-            # print "found"
-            break
+	visit_cost = visit[1] + ManhattanHeuristic(visit[0], goal)
+        found = 0
 
-        if visit not in Visited:
-            Visited.append(visit)
-            children = getNeighbours(visit, r, c)
-            # print children
-            while (children):
-                child = children.pop(0)
-                if child not in Visited:
-                    if child not in ToVisit_:
-                        ToVisit.put(child)
-                        ToVisit_.append(child)
-                        X[child] = visit
+	if visit[0] in Food:
+		food_r, food_c = Food.pop()
+		Food.append((tuple((food_r, food_c)) , visit_cost))
+		break
 
-    node = Food.pop()
-    start = tuple((pacman_r, pacman_c))
-    while(node != start):
-        Route.append(node)
-        node = X[node]
-    Route.append(start)
+	for i in Visited:
+		if(i[0] == visit[0]):
+			found = 1
+			if(i[1] > visit_cost + ManhattanHeuristic(visit[0], goal)):
+				Visited.remove(i)
+				children = getNeighbours(visit[0], r, c)
+				for child in children:
+					child = tuple((child, visit_cost + 1 + ManhattanHeuristic(visit[0], goal)))
+					X[child] = visit
+					ToVisit.put(child, visit_cost + 1 + ManhattanHeuristic(visit[0], goal))
+				Visited.append(visit)
+
+			else:
+				continue
+
+	if(found == 0):
+		children = getNeighbours(visit[0], r, c)
+		for child in children:
+			child = tuple((child, visit_cost + 1 + ManhattanHeuristic(visit[0], goal)))
+			X[child] = visit
+			ToVisit.put(child, visit_cost + 1 + ManhattanHeuristic(visit[0], goal))
+		Visited.append(visit)
+
+    node  = Food.pop()
+    start = tuple((tuple((pacman_r, pacman_c)), 0))
+    while(node[0] != start[0]):
+	Route.append(node[0])
+	node = X[node]
+
+    Route.append(start[0])
     printer()
 
 
@@ -183,5 +221,5 @@ for i in xrange(0, r):
 
 
 # print Walls
-#nextMove(r, c, pacman_r, pacman_c, food_r, food_c, grid)
-dfs(r, c, pacman_r, pacman_c, food_r, food_c, grid)
+nextMove(r, c, pacman_r, pacman_c, food_r, food_c, grid)
+#dfs(r, c, pacman_r, pacman_c, food_r, food_c, grid)
